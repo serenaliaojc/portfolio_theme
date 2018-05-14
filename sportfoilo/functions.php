f90b42089d400bb580afe79523b09f9eec53cfbf
serenaliaojc <36046972+serenaliaojc@users.noreply.github.com>
2018-05-14 18:35:38 -0400

    add sass folder

--- sportfoilo/functions.php
+++ sportfoilo/functions.php
@@ -122,9 +122,9 @@ add_action( 'widgets_init', 'sportfolio_widgets_init' );
 function sportfolio_scripts() {
 	wp_enqueue_style( 'sportfolio-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'sportfolio-navigation', get_template_directory_uri() . '/js/navigation.js', array(), '20151215', true );
+	wp_enqueue_script( 'sportfolio-navigation', get_template_directory_uri() . '/assets/js/navigation.js', array(), '20151215', true );
 
-	wp_enqueue_script( 'sportfolio-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), '20151215', true );
+	wp_enqueue_script( 'sportfolio-skip-link-focus-fix', get_template_directory_uri() . '/assets/js/skip-link-focus-fix.js', array(), '20151215', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
